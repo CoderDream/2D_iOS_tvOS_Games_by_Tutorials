5d3fcf49634625b114457a1d299f432ed81a0c8d
coderdream <coderdream@coderdreamdemacbook-pro.local>
2018-12-21 00:08:18 +0800

    add MyUtils

---
+++ Chapter01/ZombieConga/ZombieConga/MyUtils.swift
@@ -0,0 +1,68 @@
+//
+//  MyUtils.swift
+//  ZombieConga
+//
+//  Created by coderdream on 2018/12/20.
+//  Copyright © 2018 coderdream. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func += (left: inout CGPoint, right: CGPoint) {
+    left = left + right
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func -= (left: inout CGPoint, right: CGPoint) {
+    left = left + right
+}
+
+func * (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x * right.x, y: left.y * right.y)
+}
+
+func *= (left: inout CGPoint, right: CGPoint) {
+    left = left * right
+}
+
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func *= (point: inout CGPoint, scalar: CGFloat) {
+    point = point * scalar
+}
+
+func / (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x / right.x, y: left.y / right.y)
+}
+
+func /= (left: inout CGPoint, right: CGPoint) {
+    left = left / right
+}
+
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar, y: point.y * scalar)
+}
+
+func /= (point: inout CGPoint, scalar: CGFloat) {
+    point = point / scalar
+}
+
+#if !(arch(x86_64) || arch(arm64))
+func atan2(y: CGFloat, x: CGFloat) -> CGFloat {
+    return CGFloat(atan(atan2f(Float(y), Float(x))))
+}
+
+func sqrt(a: CGFloat) -> CGFloat {
+    return CGFloat(sqrtf(Float(a)))
+}
+#endif
