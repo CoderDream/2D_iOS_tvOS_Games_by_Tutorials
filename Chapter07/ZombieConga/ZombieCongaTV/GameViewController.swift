908ed7d3bcdf504a9901466dc2b4dc800771743b
CoderDream <coderdream@gmail.com>
2018-12-24 22:13:18 +0800

    Chapter07 7.4 添加一个tvOS目标

---
+++ Chapter07/ZombieConga/ZombieCongaTV/GameViewController.swift
@@ -0,0 +1,27 @@
+//
+//  GameViewController.swift
+//  ZombieCongaTV
+//
+//  Created by coderdream on 2018/12/24.
+//  Copyright © 2018 coderdream. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = MainMenuScene.init(size: CGSize(width: 2048, height: 1536))
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
+    }
+ 
+}
