0bc28fcf06443a6e73bbb8352708724bb66e539d
许林 <xulin.wh@qq.com>
2018-12-25 10:04:22 +0800

    Chapter09 9.3 第一个 Playground

---
+++ Chapter09/SpriteKitPhysicsTest/SpriteKitPhysicsTest.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let number = 0.4
+let string = "Sprite Kit is #\(5-4)"
+let numbers = Array(1...5)
+
+var j = 0
+for i in 1 ..< 10 {
+    j += i * 2
+}
