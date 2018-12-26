b9605b17c7aafb107ddbe96b93f947446c63fe83
CoderDream <coderdream@gmail.com>
2018-12-26 23:18:44 +0800

    9.8 定制形状的实体

--- Chapter09/SpriteKitPhysicsTest/SpriteKitPhysicsTest.playground/Contents.swift
+++ Chapter09/SpriteKitPhysicsTest/SpriteKitPhysicsTest.playground/Contents.swift
@@ -37,6 +37,23 @@ scene.addChild(triangle)
 // 9.5 圆形实体
 circle.physicsBody = SKPhysicsBody(circleOfRadius: circle.size.width / 2)
 
+// 9.7 矩形实体
+square.physicsBody = SKPhysicsBody(rectangleOf: square.frame.size)
+
+// 9.8 定制形状的实体
+// 创建虚线绘制路径
+var trianglePath = CGMutablePath()
+
+// http://www.cocoachina.com/bbs/read.php?tid-1698108.html
+// 将“虚拟画笔”移动到三角形的第一个点上（左下角）
+trianglePath.move(to: CGPoint(x: -triangle.size.width / 2, y: -triangle.size.height / 2))
+// 绘制3条线
+trianglePath.addLine(to: CGPoint(x: triangle.size.width / 2, y: -triangle.size.height / 2))
+trianglePath.addLine(to: CGPoint(x: 0, y: triangle.size.height / 2))
+trianglePath.addLine(to: CGPoint(x: -triangle.size.width / 2, y: -triangle.size.height / 2))
+// 创建三角形实体
+triangle.physicsBody = SKPhysicsBody(polygonFrom: trianglePath)
+
 // 延时2秒后恢复重力作用
 DispatchAfter(after: 2.0) {
     scene.physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
