4e89fb0b33ca465dc64e9aa7250e7ccb9e486c5c
coderdream <coderdream@coderdreamdeMacBook-Pro.local>
2019-01-09 23:39:02 +0800

    11.3 加载关卡

--- Chapter11/starter/CatNap/GameViewController.swift
+++ Chapter11/starter/CatNap/GameViewController.swift
@@ -25,46 +25,47 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    if let view = self.view as! SKView? {
-      // Load the SKScene from 'GameScene.sks'
-      if let scene = SKScene(fileNamed: "GameScene") {
-        // Set the scale mode to scale to fit the window
-        scene.scaleMode = .aspectFill
-
-        // Present the scene
-        view.presentScene(scene)
-      }
-
-      view.ignoresSiblingOrder = false
-
-      view.showsFPS = true
-      view.showsNodeCount = true
-      view.showsPhysics = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            //if let scene = SKScene(fileNamed: "GameScene") {
+            if let scene = GameScene.level(levelNum: 1) {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = false
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+            view.showsPhysics = true
+        }
     }
-  }
-
-  override var shouldAutorotate: Bool {
-    return true
-  }
-
-  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-    if UIDevice.current.userInterfaceIdiom == .phone {
-      return .allButUpsideDown
-    } else {
-      return .all
+    
+    override var shouldAutorotate: Bool {
+        return true
     }
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Release any cached data, images, etc that aren't in use.
-  }
-
-  override var prefersStatusBarHidden: Bool {
-    return true
-  }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
